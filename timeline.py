2ce247d7839619616b0fb24dfc3eb860288b1bd3
Angie Chang <pedrop317@hotmail.com>
2013-07-24 12:33:28 -0700

    second commit

--- timeline.py
+++ timeline.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request
 from model import User, session
-import model # why do we call things model?!
+import model # why do we call things model
 
 app = Flask(__name__)
 
